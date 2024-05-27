83cb576e1825c86f690ae5a5c2a1397fcf111698
adryanbartolome <adryanbartolome8123@gmail.com>
2024-05-26 21:07:14 -0700

    Created Add player jsx and can add player to api

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import GetPlayers from '../GetPlayers'
-import AddPlayer from '../AddPlayer'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import GetPlayers from "../GetPlayers";
+import AddPlayer from "../AddPlayer";
+import SinglePlayer from "../SinglePlayer";
 
 const Router = () => {
   return (
     <Routes>
-        <Route path='/' element={<h1>Home Page</h1>}/>
-        <Route path='/players' element={<GetPlayers/>} />
-        <Route path='/addplayer' element={<AddPlayer/>} />
+      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="/players" element={<GetPlayers />} />
+      <Route path="/addplayer" element={<AddPlayer />} />
+      <Route path="/player/:id" element={<SinglePlayer />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
